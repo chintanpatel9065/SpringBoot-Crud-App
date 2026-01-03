1faedbb0fc1df7e83da2c84e04cd1bc6230318b8
chintanpatel <chintanpatel9065@gmail.com>
2026-01-04 04:54:57 +0530

    Implement DepartmentServiceImpl with CRUD operations

---
+++ src/main/java/org/chintanpatel/springbootcrudapp/department/DepartmentServiceImpl.java
@@ -0,0 +1,38 @@
+package org.chintanpatel.springbootcrudapp.department;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+@Service("departmentService")
+public class DepartmentServiceImpl implements DepartmentService {
+
+    private final DepartmentRepository departmentRepository;
+
+    public DepartmentServiceImpl(DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
+    }
+
+    @Override
+    public Department addDepartment(Department department) {
+        return departmentRepository.save(department);
+    }
+
+    @Override
+    public List<Department> getAllDepartmentList() {
+        return departmentRepository.findAll();
+    }
+
+    @Override
+    public Optional<Department> getDepartmentById(Long departmentId) {
+        return departmentRepository.findById(departmentId);
+    }
+
+    @Override
+    public void deleteDepartmentById(Long departmentId) {
+        departmentRepository.deleteById(departmentId);
+    }
+}
