a99d0fd54e1c77f109bd78b5ce25dfc058a5ca7b
chintanpatel <chintanpatel9065@gmail.com>
2026-01-04 04:52:48 +0530

    Add DepartmentService interface with CRUD method definitions

---
+++ src/main/java/org/chintanpatel/springbootcrudapp/department/DepartmentService.java
@@ -0,0 +1,15 @@
+package org.chintanpatel.springbootcrudapp.department;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DepartmentService {
+
+    Department addDepartment(Department department);
+
+    List<Department>getAllDepartmentList();
+
+    Optional<Department>getDepartmentById(Long departmentId);
+
+    void deleteDepartmentById(Long departmentId);
+}
